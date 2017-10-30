32e1eb292143b30fb07d7b342af62178d77cfef6
luizbweb <luizbweb@gmail.com>
2017-10-30 19:48:27 -0200

    Upload images and create directories

--- test.js
+++ test.js
@@ -3,19 +3,34 @@ window.onload = function() {
 	upload();
 };
 
+function getUrlVars()
+{
+    var vars = [], hash;
+    var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+    for(var i = 0; i < hashes.length; i++)
+    {
+        hash = hashes[i].split('=');
+        vars.push(hash[0]);
+        vars[hash[0]] = hash[1];
+    }
+    return vars;
+}
 // Load a panorama stored on the user's computer
 function upload() {
 	// Retrieve the chosen file and create the FileReader object
 	// var file = document.getElementById('pano').files[0];
 	// var file = 'examples/snow.jpg';
 	// var reader = new FileReader();
+	// Obtaining the value from GET
+	var image = getUrlVars();
+	console.log(image['i']);
 
 	// reader.onload = function() {
 		PSV = new PhotoSphereViewer({
 			// Panorama, given in base 64
 			// panorama: reader.result,
-			// panorama: 'http://refstecnologiaservices.com.br/homolog/etic-360/escritorio.jpg',
-			panorama: 'examples/sun.jpg',
+			// panorama: 'ex/sun.jpg',
+			panorama: image['i'],
 			// panorama: 'examples/escritorio.jpg',
 
 			// Container
